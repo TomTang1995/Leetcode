4bf5204c94e19872421abbbcb455a9db72a42e10
TomTang1995 <421940579@qq.com>
2019-07-17 22:13:30 +0800

    update 2019.07.17

--- src/leetcode/Q023.java
+++ src/leetcode/Q023.java
@@ -18,24 +18,9 @@ public class Q023
 
     private ListNode mergeList(ListNode list1, ListNode list2)
     {
-        if(list1 == null)
-            return list2;
-        else if(list2 == null)
-            return list1;
+        ListNode tempHead = new ListNode(0);
+        ListNode current = tempHead;
 
-        ListNode head, current;
-        if(list1.val < list2.val)
-        {
-            head = list1;
-            list1 = list1.next;
-        }
-        else
-        {
-            head = list2;
-            list2 = list2.next;
-        }
-
-        current = head;
         while(list1!=null && list2!=null)
         {
             if(list1.val < list2.val)
@@ -52,6 +37,6 @@ public class Q023
         }
 
         current.next = list1==null ? list2 : list1;
-        return head;
+        return tempHead.next;
     }
 }
