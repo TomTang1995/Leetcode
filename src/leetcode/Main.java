77d1bac721c537cbf7c04aa7df5288997bd2d53c
TomTang1995 <421940579@qq.com>
2019-10-14 19:43:43 +0800

    update 2019.10.14

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -7,6 +7,19 @@ public class Main
 {
     public static void main(String args[])
     {
-        System.out.println("Hello world!");
+        List<int[]> list = new ArrayList<>();
+        list.add(new int[]{1,2});
+        list.add(new int[]{3,4});
+
+        int twoDimension[][] = new int[list.size()][list.get(0).length];
+        list.toArray(twoDimension);
+
+        for(int i = 0;i < twoDimension.length;i++)
+        {
+            for(int j = 0;j < twoDimension[0].length;j++)
+                System.out.print(twoDimension[i][j] + " ");
+
+            System.out.println();
+        }
     }
 }
